585713108615e070ae12964eb3edce52589d9893
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-10-11 22:37:33 -0500

    added product.js return with props

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,7 +1,17 @@
 function Product(props) {
     return (
         <div>
-
+            <div>
+                <img src={props.image} alt={props.productName} />
+            </div>
+            <div>
+                <h2>{props.productName}</h2>
+                <h3>{props.description}</h3>
+            </div>
+            <div>
+                <h2>{props.price}</h2>
+                {/* <button>Add To Cart</button> */}
+            </div>
         </div>
     )
 }
