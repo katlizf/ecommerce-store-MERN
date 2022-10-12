af5a28ea7d4f137045d922a3f714231bebfd736c
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-10-11 21:56:51 -0500

    added switch from react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import {Route} from 'react-router-dom'
+import {Route, Redirect, Switch} from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 
 function App() {
     return (
-        <div >
+        <Switch>
             <Route path='/' exact>
                 <Home />
             </Route>
-        </div>
+            <Redirect to='/' />
+        </Switch>
     )
 }
 
 export default App
+
+// switch: whenever a fitting route is encountered, it will not evaluate the lines after
