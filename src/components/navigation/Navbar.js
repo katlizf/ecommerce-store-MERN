81a08159540db601e68e300e0440be00ab281ea4
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-10-13 15:17:39 -0500

    added SideNav to navbar.js

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,23 +1,31 @@
 import MainHeader from "./MainHeader"
 import {Link} from 'react-router-dom'
 import NavLinks from "./NavLinks"
+import SideNav from "./SideNav"
 import './Navbar.scss'
 
 function Navbar(props) {
     return (
-        <MainHeader>
-            <button className="menu-btn">
-                <span />
-                <span />
-                <span />
-            </button>
-            <h1 className="main-title">
-                <Link to='/'>Weebs-R-Us</Link>                
-            </h1>
-            <nav>
-                <NavLinks />
-            </nav>
-        </MainHeader>
+        <div>
+            <SideNav >
+                <nav className="side-nav">
+                    <NavLinks />         
+                </nav>
+            </SideNav>
+            <MainHeader>
+                <button className="menu-btn">
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className="main-title">
+                    <Link to='/'>Weebs-R-Us</Link>                
+                </h1>
+                <nav className="header-nav">
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </div>
     )
 }
 
