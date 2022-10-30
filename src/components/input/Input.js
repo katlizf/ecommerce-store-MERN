488da4fc1eb14b03e7644b75f9b36a5686110099
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-10-30 18:56:57 -0500

    created useReducer to help manage state for input and changed nested styling

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,13 +1,39 @@
+import {useReducer} from 'react'
+
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                value: action.val,
+                isValid: true
+                // need to replace with validation logic later
+            };
+        default:
+            return state;
+    }
+}
+
 function Input(props) {
+
+    const [inputState, dispatch] = useReducer(inputReducer, {value: '', isValid: false})
+
+    const changeHandler = e => {
+        dispatch({type: 'CHANGE', val: e.target.value})
+    }
+
     return (
-        <div className={'form-control'}>
+        <div className={`form-control ${!inputState.isValid && 'form-control--invalid'}`}>
             <label htmlFor={props.id}>
                 {props.label}
             </label>
             <input
                 id={props.id}
                 type={props.type}
-                placeholder={props.placeholder} />
+                placeholder={props.placeholder}
+                onChange={changeHandler}
+                value={inputState.value} />
+            {!inputState.isValid && <p>{props.errorText}</p>}
         </div>
     )
 }
