b462225285e4aae557ec43b94d43597ed25db080
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-01 14:49:28 -0500

    added additional inputs for register mode

--- src/pages/user/Auth.js
+++ src/pages/user/Auth.js
@@ -1,7 +1,7 @@
 import Input from "../../components/formElements/input/Input"
 import Button from '../../components/formElements/button/Button'
 import {useCallback, useReducer, useState} from "react"
-import {VALIDATOR_REQUIRE, VALIDATOR_EMAIL, VALIDATOR_PASSWORD} from '../../components/util/Validators'
+import {VALIDATOR_REQUIRE, VALIDATOR_EMAIL, VALIDATOR_PASSWORD, VALIDATOR_MINLENGTH, VALIDATOR_MAXLENGTH} from '../../components/util/Validators'
 
 const registerReducer = (state, action) => {
     
@@ -73,18 +73,52 @@ function Auth() {
             {!isLoginMode && 
                 <div>
                     <Input
-                    id="fName"
-                    type="text"
-                    label="First Name: "
-                    validators={[VALIDATOR_REQUIRE]}
-                    errorText="Please enter your first name."
-                    onInput={userInputHandler} />
+                        id="fName"
+                        type="text"
+                        label="First Name: "
+                        validators={[VALIDATOR_REQUIRE]}
+                        errorText="Please enter your first name."
+                        onInput={userInputHandler} />
                     <Input
                         id="lName"
                         type="text"
                         label="Last Name: "
                         validators={[VALIDATOR_REQUIRE]}
                         errorText="Please enter your last name."
+                        onInput={userInputHandler} />
+                    <Input
+                        id="stAddress"
+                        type="text"
+                        label="Street Address: "
+                        validators={[VALIDATOR_REQUIRE]}
+                        errorText="Please enter your street address."
+                        onInput={userInputHandler} />
+                    <Input
+                        id="aptEtc"
+                        type="text"
+                        label="Apt, suite, etc. (optional): "
+                        placeholder='Apt. 123'
+                        onInput={userInputHandler} />
+                    <Input
+                        id="city"
+                        type="text"
+                        label="City: "
+                        validators={[VALIDATOR_REQUIRE]}
+                        errorText="Please enter the city name."
+                        onInput={userInputHandler} />
+                    <Input
+                        id="state"
+                        type="text"
+                        label="State: "
+                        validators={[VALIDATOR_MINLENGTH(2), VALIDATOR_MAXLENGTH(2)]}
+                        errorText="Please enter your state's abbreviation (2 letters)."
+                        onInput={userInputHandler} />
+                    <Input
+                        id="zipCode"
+                        type="text"
+                        label="Zip Code: "
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        errorText="Please enter your 5-digit zip code."
                         onInput={userInputHandler} />
                 </div>
             }
@@ -100,7 +134,7 @@ function Auth() {
                 type="text"
                 label="Password: "
                 validators={[VALIDATOR_PASSWORD]}
-                errorText="Please enter your password."
+                errorText="Please enter your password. It should include at least 1 uppercase, 1 lowercase, 1 number, & 1 special character."
                 onInput={userInputHandler} />
             <Button type="submit" disabled={!formState.isValid}>{isLoginMode ? 'Register' : 'Login'}</Button>
         </form>
