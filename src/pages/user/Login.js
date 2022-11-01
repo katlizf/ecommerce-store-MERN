0e442af25f7f103fb732d725907ca4a269227ab4
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-01 13:14:47 -0500

    changed validation for inputs & added handler for login form to work along with useEffect in input component

--- src/pages/user/Login.js
+++ src/pages/user/Login.js
@@ -1,16 +1,32 @@
 import Input from "../../components/formElements/input/Input"
 import PageContainer from '../../components/pageContainer/PageContainer'
-import {VALIDATOR_REQUIRE} from '../../components/util/Validators'
+import {useCallback} from "react"
+import {VALIDATOR_MINLENGTH} from '../../components/util/Validators'
 
 function Login() {
+
+    const userInputHandler = useCallback((id, value, isValid) => {
+
+    }, [])
+    // using useCallback to prevent an infinite loop with the useEffect in the input component
+
     return (
         <PageContainer className="login">
           <form>
             <Input
+                id="fName"
                 type="text"
                 label="First Name: "
-                validators={[VALIDATOR_REQUIRE()]}
-                errorText="Please enter a valid response." />
+                validators={[VALIDATOR_MINLENGTH(1)]}
+                errorText="Please enter your first name."
+                onInput={userInputHandler} />
+            <Input
+                id="lName"
+                type="text"
+                label="Last Name: "
+                validators={[VALIDATOR_MINLENGTH(1)]}
+                errorText="Please enter your last name."
+                onInput={userInputHandler} />
         </form>  
         </PageContainer>
         
