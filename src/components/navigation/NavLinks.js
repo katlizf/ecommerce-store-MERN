d175430fb4abaac703f10507a5b892578a72aaf3
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-03 18:24:46 -0500

    set My Profile nav-link to only show when a user i s logged in

--- src/components/navigation/NavLinks.js
+++ src/components/navigation/NavLinks.js
@@ -1,6 +1,11 @@
+import {useContext} from 'react'
 import {NavLink} from 'react-router-dom'
+import {AuthContext} from '../context/AuthContext'
 
 function NavLinks() {
+
+    const auth = useContext(AuthContext)
+
     return (
         <ul className='nav-links'>
             <li>
@@ -12,12 +17,14 @@ function NavLinks() {
             <li>
                 <NavLink to='/Collectables'>Collectables</NavLink>
             </li>
-            <li>
+            {!auth.isLoggedIn && <li>
                 <NavLink to='/Auth'>Login</NavLink>
             </li>
-            <li>
+            }
+            {auth.isLoggedIn && <li>
                 <NavLink to='/Profile'> My Profile</NavLink>
             </li>
+            }
         </ul>
     )
 }
