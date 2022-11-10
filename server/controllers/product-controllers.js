2ec8df4185e9c9428d7e6b5a0938c47e8866f31b
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-10 15:45:14 -0600

    created controller files and added middleware func tions from product-routes to product-controllers and using them in product-routes

---
+++ server/controllers/product-controllers.js
@@ -0,0 +1,33 @@
+const HttpError = require('../models/http-error')
+const {get} = require('../routes/product-routes')
+
+const DUMMY_PLACES = [
+    {
+        id: '1',
+        productName: 'Sweatshirt',
+        description: 'Black',
+        price: 29.99,
+        anime: 1,
+        image: 'https://images.shirtspace.com/large/v2klhrD32Mlj8xUCYUsnWg%3D%3D/250529/12388-soffe-b9289-soffe-youth-classic-hooded-sweatshirt-front-black.jpg',
+        type: 1
+    }
+]
+
+const getAllProducts = (req, res, next) => {
+    const products = DUMMY_PLACES
+    res.json({products})
+}
+
+const getProductById = (req, res, next) => {
+    const productId = req.params.pid
+    const product = DUMMY_PLACES.find(p => {
+        return p.id === productId
+    })
+    if (!product) {
+        return next(new HttpError('Could not find a product for the product id.', 404))
+    }
+    res.json({product})
+}
+
+exports.getAllProducts = getAllProducts
+exports.getProductById = getProductById
--- server/routes/product-routes.js
+++ server/routes/product-routes.js
@@ -1,37 +1,13 @@
 const express = require('express')
 
-const HttpError = require('../models/http-error')
+const productControllers = require('../controllers/product-controllers')
 
 const router = express.Router()
 
 
-const DUMMY_PLACES = [
-    {
-        id: '1',
-        productName: 'Sweatshirt',
-        description: 'Black',
-        price: 29.99,
-        anime: 1,
-        image: 'https://images.shirtspace.com/large/v2klhrD32Mlj8xUCYUsnWg%3D%3D/250529/12388-soffe-b9289-soffe-youth-classic-hooded-sweatshirt-front-black.jpg',
-        type: 1
-    }
-]
+router.get('/', productControllers.getAllProducts)
 
-router.get('/', (req, res, next) => {
-    const products = DUMMY_PLACES
-    res.json({products})
-})
-
-router.get('/:pid', (req, res, next) => {
-    const productId = req.params.pid
-    const product = DUMMY_PLACES.find(p => {
-        return p.id === productId
-    })
-    if (!product) {
-        return next(new HttpError('Could not find a product for the product id.', 404))
-    }
-    res.json({product})
-})
+router.get('/:pid', productControllers.getProductById)
 
 
 module.exports = router
