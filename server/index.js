8a3bda351d7a29678a2fc0a08c167e87c25a936e
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-10 15:05:46 -0600

    using productRoutes in middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const productRoutes = require('./routes/product-routes')
+
 const app = express()
 
 
-
+app.use('/api/products', productRoutes)
 
 
 app.listen(5000)
