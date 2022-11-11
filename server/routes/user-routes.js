3bd704f77db2aeaeeed94a72d2847eec4201893d
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-11 13:05:19 -0600

    creating user routes

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,1 +1,15 @@
 const express = require('express')
+
+const usersController = require('../controllers/user-controllers')
+
+const router = express.Router()
+
+
+router.get('/', usersController.getUsers)
+
+router.post('/signup', usersController.signup)
+
+router.post('/login', usersController.login)
+
+
+module.exports = router
