b2b9242c3527cc9be474e5a27daae874f9812048
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-22 13:44:06 -0600

    created products schema

---
+++ server/models/product.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+
+const productsSchema = new Schema ({
+    productName: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    anime: {type: String, required: true},
+    image: {type: String, required: true},
+    type: {type: String, required: true},
+})
+
+module.exports = mongoose.model('Product', productsSchema)
