da45fb697f4bb5db72f603439b607d4030983abb
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-02 23:07:38 -0600

    created cart model, may need to alter later

---
+++ server/models/cart.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const cartSchema = new Schema({
+    product: {type: mongoose.Types.ObjectId, ref: 'Product', require: true},
+    user: {type: mongoose.Types.ObjectId, ref: 'User', require: true}
+})
+
+module.exports = mongoose.model('Cart', cartSchema)
