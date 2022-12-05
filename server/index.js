ea69f61c3927581d04a54b83d824c941722a20a4
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-04 19:01:18 -0600

    needed handle CORS errors

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ app.use('/api/products', productRoutes)
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    next()
+})
+// for handling CORS errors
+
 app.use('/api/users', userRoutes)
 
 app.use('/api/anime', animeRoutes)
