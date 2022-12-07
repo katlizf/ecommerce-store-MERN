8e45534b69143f92e1610f745651f01728c8c827
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-06 22:44:55 -0600

    updated login controller to send back the user's data

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -98,7 +98,7 @@ const login = async (req, res, next) => {
         return next(error)
     }
 
-    res.json({message: 'Logged in!'})
+    res.status(200).json({existingUser: existingUser.toObject({getters: true})})
 }
 
 // exports.getUsers = getUsers
