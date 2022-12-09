c1f501e81dd3ba8f2ce861d064d1683eee042f95
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-08 22:08:39 -0600

    working on profile page and request

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,8 +1,38 @@
+import React, {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {useHttpClient} from '../../hooks/HttpHook'
+import ErrorModel from '../../components/uiElements/ErrorModal'
+import LoadingSpinner from '../../components/uiElements/LoadingSpinner'
+import UserDetails from '../../components/user/UserDetails'
+
 function Profile() {
+
+    const [userProfile, setUserProfile] = useState()
+    const {isLoading, error, sendRequest, clearError} = useHttpClient
+
+    const userId = useParams().userId
+
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                const response = await sendRequest(`http://localhost:5000/api/users/user/${userId}`)
+
+                setUserProfile(response.user)
+                console.log(response)
+                console.log(userProfile)
+            } catch (err) { }
+        }
+        fetchUserProfile()
+    }, [sendRequest, userId])
+
     return (
-        <div>
-            Profile
-        </div>
+        <React.Fragment>
+            <ErrorModel error={error} onClear={clearError} />
+            {isLoading && <LoadingSpinner />}
+            <div>
+                <UserDetails {...userProfile} /> 
+            </div>            
+        </React.Fragment>
     )
 }
 
