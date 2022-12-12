313b1bc194ce3799b361278e67decab00391f9df
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-11 21:34:23 -0600

    created useEffect to fetch all collectables to sen d to products component

--- src/pages/collectables/Collectables.js
+++ src/pages/collectables/Collectables.js
@@ -1,21 +1,35 @@
+import React, {useEffect, useState} from 'react'
+import {useHttpClient} from '../../hooks/HttpHook'
 import Products from "../../components/products/Products"
+import LoadingSpinner from '../../components/uiElements/LoadingSpinner'
+import ErrorModal from '../../components/uiElements/ErrorModal'
 
 function Collectables() {
 
-    const PRODUCTS = [{
-        id: '2',
-        productName: 'Figurine',
-        description: 'Dimensions 7" Tall',
-        price: 39.99,
-        anime: 2,
-        image: 'Image',
-        type: 2
-    }]
+    const [allCollectables, setAllCollectables] = useState()
+    const {isLoading, error, sendRequest, clearError} = useHttpClient()
+
+    useEffect(() => {
+        const fetchCollectables = async () => {
+            try {
+                const response = await sendRequest('http://localhost:5000/api/products/collectables')
+
+                setAllCollectables(response.collectables)
+            } catch (err) {}
+        }
+        fetchCollectables()
+    }, [sendRequest])
 
     return (
-        <div>
-            <Products items={PRODUCTS} />
-        </div>
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && <LoadingSpinner />}
+            {!isLoading && allCollectables &&
+                <div>
+                    <Products items={allCollectables} />
+                </div>            
+            }
+        </React.Fragment>
     )
 }
 
