080e85837951c9cf24b55c4bb037671cc09dacc6
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-11 21:36:38 -0600

    updated Products component to display each product

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -6,26 +6,25 @@ function Products(props) {
     if (props.items.length === 0) {
         return (
             <div>
-                <h2>No Products Found</h2>
+                <h2>No Products Found.</h2>
             </div>
         )
     }
 
     return (
         <PageContainer>
-            {props.items.map(product => {
-                return (
+            <ul>
+              {props.items.map(product => (
                     <Product
                         key={product.id}
                         id={product.id}
                         productName={product.productName}
                         description={product.description}
                         price={product.price}
-                        anime={product.anime}
                         image={product.image}
-                        type={product.type} />
-                )
-            })}
+                    />
+                ))}  
+            </ul>
         </PageContainer>
     )
 }
