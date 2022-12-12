36c6b8f1954c2136952443bafd2a98eef0d7288d
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-11 21:43:44 -0600

    created useEffect to fetch all apparel to send to Products component

--- src/pages/apparel/Apparel.js
+++ src/pages/apparel/Apparel.js
@@ -1,21 +1,35 @@
+import React, {useEffect, useState} from 'react'
+import {useHttpClient} from '../../hooks/HttpHook'
 import Products from "../../components/products/Products"
+import LoadingSpinner from '../../components/uiElements/LoadingSpinner'
+import ErrorModal from '../../components/uiElements/ErrorModal'
 
 function Apparel() {
 
-    const PRODUCTS = [{
-        id: '1',
-        productName: 'Sweatshirt',
-        description: 'Grey',
-        price: 19.99,
-        anime: 1,
-        image: 'Image',
-        type: 1
-    }]
+    const [allApparel, setAllApparel] = useState()
+    const {isLoading, error, sendRequest, clearError} = useHttpClient()
+
+    useEffect(() => {
+        const fetchApparel = async () => {
+            try {
+                const response = await sendRequest('http://localhost:5000/api/products/apparel')
+
+                setAllApparel(response.apparel)
+            } catch (err) {}
+        }
+        fetchApparel()
+    }, [sendRequest])
 
     return (
-        <div>
-            <Products items={PRODUCTS} />
-        </div>
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && <LoadingSpinner />}
+            {!isLoading && allApparel &&
+                <div>
+                    <Products items={allApparel} />
+                </div>
+            }
+        </React.Fragment>
     )
 }
 
