9204b98bc782883fbd2312627b20445066022364
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-14 20:05:12 -0600

    set up delete request, confirm/cancel modal, and u pon delete user is logged out and back to home page

--- src/components/user/UserDetails.js
+++ src/components/user/UserDetails.js
@@ -1,10 +1,12 @@
 import React, {useContext, useState} from 'react'
 import {AuthContext} from '../../context/AuthContext'
 import {useHttpClient} from '../../hooks/HttpHook'
+import {useHistory} from 'react-router-dom'
 import PageContainer from '../pageContainer/PageContainer'
 import Button from '../formElements/button/Button'
 import ErrorModal from '../uiElements/ErrorModal'
 import LoadingSpinner from '../uiElements/LoadingSpinner'
+import Modal from '../uiElements/Modal'
 
 
 function UserDetails({id, fName, lName, email, password, address, city, state, zipCode}) {
@@ -12,9 +14,46 @@ function UserDetails({id, fName, lName, email, password, address, city, state, z
     const {isLoading, error, sendRequest, clearError} = useHttpClient()
     const auth = useContext(AuthContext)
     const [showConfirmModal, setShowConfirmModal] = useState(false)
+    let history = useHistory()
+
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true)
+    }
+
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false)
+    }
+
+    const confirmDeleteHandler = async () => {
+        setShowConfirmModal(false)
+        try {
+            await sendRequest(`http://localhost:5000/api/users/user/${id}`, 'DELETE')
+            history.push('/')
+            auth.logout()
+        } catch (err) {}
+    }
 
     return (
         <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && <LoadingSpinner />}
+            <Modal
+                show={showConfirmModal}
+                onCancel={cancelDeleteHandler}
+                header='Are you sure?'
+                footer={
+                    <React.Fragment>
+                        <Button onClick={cancelDeleteHandler}>
+                            Cancel
+                        </Button>
+                        <Button onClick={confirmDeleteHandler}>
+                            Delete
+                        </Button>
+                    </React.Fragment>
+                }
+            >
+                <p>Do you want to proceed and delete your account? Please note that this cannot be undone.</p>
+            </Modal>
             <h1>Profile</h1>
             <h2>{fName}</h2>
             <h2>{lName}</h2>
@@ -26,9 +65,9 @@ function UserDetails({id, fName, lName, email, password, address, city, state, z
             <h2>{zipCode}</h2>
             <div>
                 <Button to={`/users/user/${id}`}>Edit</Button>
-                {/* <Button onClick={deleteWarningHandler}>Delete</Button> */}
-            </div>            
-        </React.Fragment>
+                <Button onClick={showDeleteWarningHandler}>Delete</Button>
+            </div>
+        </React.Fragment >
     )
 }
 
