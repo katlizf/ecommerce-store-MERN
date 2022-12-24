7e4a697cae3cad67b0c81a8b415bdd5b07469419
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-12-23 21:24:29 -0600

    added missing /'s

--- server/middleware/file-upload.js
+++ server/middleware/file-upload.js
@@ -3,15 +3,15 @@ const uuid = require('uuid')
 
 const MIME_TYPE_MAP = {
     'image/png': 'png',
-    'image.jpg': 'jpg',
-    'image.jpeg': 'jpeg'
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg'
 }
 
 const fileUpload = multer({
     limits: 500000,
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, 'uploads/images')
+            cb(null, '/uploads/images')
         },
         fileName: (req, file, cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype]
