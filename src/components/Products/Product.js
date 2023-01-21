a8894d76bfd75d0069389d776426c04bc08996f8
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-01-20 19:52:11 -0600

    created 2 sections for styling purposes

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,15 +1,19 @@
 import Button from "../formElements/Button"
 
 function Product(props) {
-    return (
-        <div className="product-card">
-            <img className="image" src={props.image} alt={props.productName} />
-            <h2 className="name">{props.productName}</h2>
-            <h3 className="description">{props.description}</h3>
-            <h2 className="price">{props.price}</h2>
-            <Button className="cart-btn">Add to Cart</Button>        
-        </div>
-    )
+	return (
+		<div className="product-card">
+            <div className="img-name-desc">
+                <img className="image" src={props.image} alt={props.productName} />
+			    <h2 className="name">{props.productName}</h2>
+			    <h3 className="description">{props.description}</h3>
+            </div>
+            <div className="price-cart">
+                <h2 className="price">{props.price}</h2>
+			    <Button className="cart-btn">Add to Cart</Button>
+            </div>
+		</div>
+	)
 }
 
 export default Product
