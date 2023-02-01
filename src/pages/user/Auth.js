b4d10e2b5e700395bf51c342b7c8c2ae2015f5cb
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-01-31 21:35:22 -0600

    added image to submitHandler & ImageUpload comopon ent

--- src/pages/user/Auth.js
+++ src/pages/user/Auth.js
@@ -17,6 +17,7 @@ import PageContainer from "../../components/pageContainer/PageContainer"
 import ImageUpload from "../../components/formElements/ImageUpload"
 
 function Auth() {
+
 	const auth = useContext(AuthContext)
 	const [isLoginMode, setIsLoginMode] = useState(true)
 	const {isLoading, error, sendRequest, clearError} = useHttpClient()
@@ -80,8 +81,8 @@ function Auth() {
 						isValid: false,
 					},
 					image: {
-						value: "",
-						isValid: true,
+						value: null,
+						isValid: false,
 					},
 				},
 				false
@@ -108,22 +109,33 @@ function Auth() {
 			} catch (err) {}
 		} else {
 			try {
-				const response = await sendRequest(
-					"http://localhost:5000/api/users/signup",
-					"POST",
-					JSON.stringify({
-						fName: formState.inputs.fName.value,
-						lName: formState.inputs.lName.value,
-						email: formState.inputs.email.value,
-						password: formState.inputs.password.value,
-						address: formState.inputs.address.value,
-						city: formState.inputs.city.value,
-						state: formState.inputs.state.value,
-						zipCode: formState.inputs.zipCode.value,
-						image: formState.inputs.image.value,
-					}),
-					{"Content-Type": "application/json"}
-				)
+                const formData = new FormData()
+                formData.append('fName', formState.inputs.fName.value)
+                formData.append('lName', formState.inputs.lName.value)
+                formData.append('email', formState.inputs.email.value)
+                formData.append('password', formState.inputs.password.value)
+                formData.append('address', formState.inputs.address.value)
+                formData.append('city', formState.inputs.city.value)
+                formData.append('state', formState.inputs.state.value)
+                formData.append('zipCode', formState.inputs.zipCode.value)
+                formData.append('image', formState.inputs.image.value)
+                const response = await sendRequest('http://localhost:5000/api/users/signup', 'POST', formData)
+				// const response = await sendRequest(
+				// 	"http://localhost:5000/api/users/signup",
+				// 	"POST",
+				// 	JSON.stringify({
+				// 		fName: formState.inputs.fName.value,
+				// 		lName: formState.inputs.lName.value,
+				// 		email: formState.inputs.email.value,
+				// 		password: formState.inputs.password.value,
+				// 		address: formState.inputs.address.value,
+				// 		city: formState.inputs.city.value,
+				// 		state: formState.inputs.state.value,
+				// 		zipCode: formState.inputs.zipCode.value,
+				// 		image: formState.inputs.image.value,
+				// 	}),
+				// 	{"Content-Type": "application/json"}
+				// )
 				auth.login(response.user.id)
 			} catch (err) {}
 		}
@@ -137,11 +149,11 @@ function Auth() {
 				<form className="auth-form" onSubmit={authSubmitHandler}>
 					{!isLoginMode && (
 						<div>
-							{/* <ImageUpload
+							<ImageUpload
 								id="image"
 								onInput={inputHandler}
-								required="false"
-							/> */}
+								errorText="Please provide an image"
+							/>
 							<div className="register-name">
 								<Input
 									id="fName"
