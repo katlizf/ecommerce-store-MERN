0dec762e9cb100e36d85aa6c2af53bb675d6d6db
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-02-08 20:19:28 -0600

    planning on creating a modal to open when user cli cks on product to see details and able to select size and quantity before adding to cart

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,6 +1,15 @@
+import {useState} from "react"
 import Button from "../formElements/Button"
+import ProductModal from "./ProductModal"
 
 function Product(props) {
+
+    const [modalOpen, setModalOpen] = useState(false)
+
+    const openProductModal = e => {
+        setModalOpen(prevMode => !prevMode)
+    }
+
 	return (
 		<div className="product-card">
             <img className="image" src={props.image} alt={props.productName} />
@@ -10,10 +19,11 @@ function Product(props) {
             </div>
             <div className="price-cart">
                 <h2 className="price">${props.price}</h2>
-			    <Button className="cart-btn">
+			    <Button className="cart-btn" onClick={openProductModal}>
                     <p className="add-to-cart">Add to Cart</p>
                     <i className="icon-shopping-cart"></i>
                 </Button>
+                {modalOpen && <ProductModal />}
             </div>
 		</div>
 	)
---
+++ src/components/Products/ProductModal.js
@@ -0,0 +1,11 @@
+import Modal from "../uiElements/Modal"
+
+function ProductModal(props) {
+    return (
+        <Modal>
+           
+        </Modal>
+    )
+}
+
+export default ProductModal
