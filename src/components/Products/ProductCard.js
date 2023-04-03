85294f862ef74d597057bdd2beb84d7c6796cc40
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-04-02 19:16:52 -0500

    removed description and added productModal

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,30 +1,34 @@
 import {useState} from "react"
 import Button from "../formElements/Button"
+import Modal from "../uiElements/Modal"
 import ProductModal from "./ProductModal"
 
 function Product(props) {
 
-    const [modalOpen, setModalOpen] = useState(false)
+	const [modalOpen, setModalOpen] = useState(false)
 
-    const openProductModal = e => {
-        setModalOpen(prevMode => !prevMode)
-    }
+	const openProductModal = e => {
+		setModalOpen(true)
+	}
+
+	const closeProductModal = e => {
+		setModalOpen(false)
+	}
 
 	return (
 		<div className="product-card">
-            <img className="image" src={props.image} alt={props.productName} />
-            <div className="name-desc">
-			    <h2 className="name">{props.productName}</h2>
-			    <h3 className="description">{props.description}</h3>
-            </div>
-            <div className="price-cart">
-                <h2 className="price">${props.price}</h2>
-			    <Button className="cart-btn" onClick={openProductModal}>
-                    <p className="add-to-cart">Add to Cart</p>
-                    <i className="icon-shopping-cart"></i>
-                </Button>
-                {modalOpen && <ProductModal />}
-            </div>
+			<img className="image" src={props.image} alt={props.productName} />
+			<h2 className="name">{props.productName}</h2>
+			<div className="price-cart">
+				<h2 className="price">${props.price}</h2>
+				<Button className="cart-btn" onClick={openProductModal}>
+					<p className="add-to-cart">Add to Cart</p>
+					<i className="icon-shopping-cart"></i>
+				</Button>
+			</div>
+			<Modal show={modalOpen} onCancel={closeProductModal}>
+				<ProductModal productName={props.productName} description={props.description} price={props.price} image={props.image} />
+			</Modal>
 		</div>
 	)
 }
