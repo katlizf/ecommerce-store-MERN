c3ce9cf2b5cd85337ba7dfa96cca5563da2142ef
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-04-02 19:16:20 -0500

    creating productModal for user to see details abou t the product and choose the size of apparel before adding to cart

--- src/components/Products/ProductModal.js
+++ src/components/Products/ProductModal.js
@@ -1,10 +1,24 @@
-import Modal from "../uiElements/Modal"
+import Button from "../formElements/Button"
 
-function ProductModal(props) {
+function ProductModal({productName, description, price, image}) {
     return (
-        <Modal>
-           
-        </Modal>
+        <div className="product-modal">
+            <img className="image" src={image} alt={productName} />
+            <h2 className="name">{productName}</h2>
+			<h3 className="description">{description}</h3>
+            <div className="sizes">
+                <button type="radio" className="size" id="xs">X-small</button>
+                <button type="radio" className="size" id="s">Small</button>
+                <button type="radio" className="size" id="m">Medium</button>
+                <button type="radio" className="size" id="l">Large</button>
+                <button type="radio" className="size" id="xl">X-large</button>
+            </div>
+            <h3 className="price">{price}</h3>
+            <Button className="add-to-cart">
+                <p className="add-to-cart">Add to Cart</p>
+				<i className="icon-shopping-cart"></i>
+            </Button>
+        </div>
     )
 }
 
