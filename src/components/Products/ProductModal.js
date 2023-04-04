09f9d1f6d8f2ac33ad9ed188f49cf01e9c14cb48
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-04-03 20:21:25 -0500

    edited productModal component for styling

--- src/components/Products/ProductModal.js
+++ src/components/Products/ProductModal.js
@@ -1,25 +1,39 @@
 import Button from "../formElements/Button"
 
 function ProductModal({productName, description, price, image}) {
-    return (
-        <div className="product-modal">
-            <img className="image" src={image} alt={productName} />
-            <h2 className="name">{productName}</h2>
-			<h3 className="description">{description}</h3>
-            <div className="sizes">
-                <button type="radio" className="size" id="xs">X-small</button>
-                <button type="radio" className="size" id="s">Small</button>
-                <button type="radio" className="size" id="m">Medium</button>
-                <button type="radio" className="size" id="l">Large</button>
-                <button type="radio" className="size" id="xl">X-large</button>
-            </div>
-            <h3 className="price">{price}</h3>
-            <Button className="add-to-cart">
-                <p className="add-to-cart">Add to Cart</p>
-				<i className="icon-shopping-cart"></i>
-            </Button>
-        </div>
-    )
+	return (
+		<div className="product-modal">
+			<div className="product-details">
+				<img className="image" src={image} alt={productName} />
+				<div className="text-info">
+					<h2 className="name">{productName}</h2>
+					<h3 className="description">{description}</h3>
+					<div className="sizes">
+						<button type="radio" className="size" id="xs">
+							X-small
+						</button>
+						<button type="radio" className="size" id="s">
+							Small
+						</button>
+						<button type="radio" className="size" id="m">
+							Medium
+						</button>
+						<button type="radio" className="size" id="l">
+							Large
+						</button>
+						<button type="radio" className="size" id="xl">
+							X-large
+						</button>
+					</div>
+					<h3 className="price">${price}</h3>
+					<Button className="cart-btn">
+						<p className="add-to-cart">Add to Cart</p>
+						<i className="icon-shopping-cart"></i>
+					</Button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ProductModal
