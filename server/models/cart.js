43c007dbbf48ba1ff82ddd3a99503ceab0dac85b
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-04-07 15:58:07 -0500

    added size to cart model for apparel

--- server/models/cart.js
+++ server/models/cart.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const cartSchema = new Schema({
 	product: {type: mongoose.Types.ObjectId, ref: "Product", require: true},
 	user: {type: mongoose.Types.ObjectId, ref: "User", require: true},
+    size: {type: String}
 })
 
 module.exports = mongoose.model("Cart", cartSchema)
