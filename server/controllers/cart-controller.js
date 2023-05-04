3a75170717b73978011d98258c1af6b1c29e2255
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-05-03 20:14:44 -0500

    edited getCart function, working on addtoCart func tion

--- server/controllers/cart-controller.js
+++ server/controllers/cart-controller.js
@@ -1,6 +1,6 @@
 const HttpError = require("../models/http-error")
-const Cart = require("../models/cart")
-const {validateResult} = require ("express-validator")
+const CartProduct = require("../models/cart")
+const {validationResult} = require("express-validator")
 const mongoose = require("mongoose")
 
 const getCart = async (req, res, next) => {
@@ -8,7 +8,7 @@ const getCart = async (req, res, next) => {
     let cart
 
     try {
-        cart = await Cart.findById(userId)
+        cart = await CartProduct.find(userId('userId'))
     } catch (err) {
         const error = new HttpError(
             "Something went wrong, could not find cart.",
@@ -18,7 +18,7 @@ const getCart = async (req, res, next) => {
     }
 
     if (!cart) {
-        const error = new HttpError("Could not find any products in the cart.", 404)
+        const error = new HttpError("Could not find any products in your cart.", 404)
         return next (error)
     }
 
@@ -26,7 +26,7 @@ const getCart = async (req, res, next) => {
 }
 
 const addToCart = async (req, res, next) => {
-    const errors = validateResult(req)
+    const errors = validationResult(req)
 
     if(!errors.isEmpty()) {
         return next(
@@ -34,7 +34,45 @@ const addToCart = async (req, res, next) => {
         )
     }
 
+    const {productId, userId, quantity} = req.body
 
+    let existingUserCartProduct
+
+    try {
+        existingUserCartProduct = await CartProduct.find({
+            "userId": userId,
+            "productId": productId
+        })
+    } catch (err) {
+        const error = new HttpError(
+            "Adding product to cart failed, please try again later.", 500
+        )
+        return next(error)
+    }
+
+    if(existingUserCartProduct) {
+        const error = new HttpError(
+            "This product is already in your cart.", 422
+        )
+        return next(error)
+    }
+
+    const newCartProduct = new CartProduct({
+        productId,
+        userId,
+        quantity
+    })
+
+    try {
+        await newCartProduct.save()
+    } catch (err) {
+        const error = new HttpError(
+            "Adding product to cart failed, please try again later.", 500
+        )
+        return next(error)
+    }
+
+    res.status(201).json({cartProduct: newCartProduct.toObject({getters:true})})
 }
 
 exports.getCart = getCart
