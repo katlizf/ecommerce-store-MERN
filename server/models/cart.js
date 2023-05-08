415efbb1fee2e1344a77d3be039378359792bfb2
Katie Anderson <katie.fullstackwebdev@gmail.com>
2023-05-08 16:51:43 -0500

    created cart function in models for storing info i n the session

--- server/models/cart.js
+++ server/models/cart.js
@@ -1,12 +1,43 @@
-const mongoose = require("mongoose")
+module.exports = function Cart(oldCart) {
+    this.items = oldCart
+    this.totalQty = oldCart.totalQty
+    this.totalPrice = oldCart.totalPrice
 
-const Schema = mongoose.Schema
+    this.add = function(item, id) {
+        let storedItem = this.items[id]
+        if (!storedItem) {
+            storedItem = this.items[id] = {item: item, qty: 0, price: 0}
+            // creating a new entry
+        }
+        storedItem.qty++
+        storedItem.price = storedItem.price * storedItem.qty
+        this.totalQty++
+        this.totalPrice += storedItem.price
+    }
 
-const cartSchema = new Schema({
-	productId: {type: mongoose.Types.ObjectId, ref: "Product", require: true},
-	userId: {type: mongoose.Types.ObjectId, ref: "User", require: true},
-    quantity: {type: Number, require: true},
-    size: {type: String}
-})
+    this.generateArray = function() {
+        let arr = []
+        for (let id in this.items) {
+            arr.push(this.items[id])
+        }
+        return arr
+        // to output list of product groups
+    }
+}
+// create a new cart off the old cart when adding a new item to check if the same item id already exists then only update the qunatity instead of pushing the item
 
-module.exports = mongoose.model("Cart", cartSchema)
+// const mongoose = require("mongoose")
+// const uniqueValidator = require("mongoose-unique-validator")
+
+// const Schema = mongoose.Schema
+
+// const cartSchema = new Schema({
+// 	productId: {type: mongoose.Types.ObjectId, ref: "Product", require: true},
+// 	userId: {type: mongoose.Types.ObjectId, ref: "User", require: true},
+//     quantity: {type: Number, require: true},
+//     size: {type: String}
+// })
+
+// cartSchema.plugin(uniqueValidator)
+
+// module.exports = mongoose.model("Cart", cartSchema)
